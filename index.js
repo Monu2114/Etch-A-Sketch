62cd2a39aced0b0219a019a3b47ec6d738ea8519
Monu_2114 <monu.nanabala@gmail.com>
2024-06-23 22:58:41 +0530

    opacity as well as squares updated

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const container = document.querySelector('div');
-function displayGrid(){
-    for(let i = 0; i < 255; i++){
-        
+const button = document.querySelector('button');
+button.textContent = 'No of Squares per side?';
+let k=0;
+button.addEventListener('click', function() {
+    
+    let squares = prompt('Enter the number of squares per side');
+    container.innerHTML = '';
+    displayGrid(squares);
+});
+function displayGrid(squares= 16) {
+    for (let i = 0; i <squares; i++) {
+        for (let j = 0; j <squares; j++) {
             let div = document.createElement('div');
-            div.style.width = '25px';
-            div.style.height = '25px';
-            div.style.border = '1px solid grey';
+            div.style.width = 400 / squares + 'px';
+            div.style.height = 400 / squares + 'px';
+            div.style.border = '0.1px solid grey';
             container.appendChild(div);
-}
-}
-container.addEventListener('mouseover', function(e){
-    console.log(e.target);
-    if(e.target.class=='div.container'){
-        e.target.style.backgroundColor = 'white';
+           
     }
-    else{
-e.target.style.backgroundColor = 'black';
     }
-})
+}
+container.addEventListener('mouseover', function(e) {
+    if (e.target.classList.contains('container')) {
+        return; // Ignore hover over the container itself
+    }
+    else if(k==10)
+    {
+        k=0;
+    }
+    else
+    {
+
+    e.target.style.backgroundColor = 'black';
+    e.target.style.opacity=0.1+k*0.1;
+    k++;
+}
+});
+
 displayGrid();
